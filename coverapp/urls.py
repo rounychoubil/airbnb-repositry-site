fac83b791d1629467a5beb3c759a7d6a8cf8afb3
rounychoubil <crouny15@gmail.com>
2025-09-07 17:45:33 -0400

    ajout des link salon, sal a mange, banho

--- coverapp/urls.py
+++ coverapp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import HomeViews
+from . import views
 
 
 urlpatterns = [
-    path('',HomeViews.as_view(),name="home"),
+    path('',views.HomeViews.as_view(),name="home"),
+    path("banho/",views.BanhoView.as_view(),name='banho'),
+    path("salamange",views.SalleAmangerView.as_view(),name="sale-manger"),
+    path("salon",views.SalonView.as_view(),name='salon'),
 
 ]
--- coverapp/views.py
+++ coverapp/views.py
@@ -3,3 +3,11 @@ from django.views.generic import TemplateView
 class HomeViews(TemplateView):
     template_name='home/index.html'
 
+class SalonView(TemplateView):
+    template_name='home/salon.html'
+
+class SalleAmangerView(TemplateView):
+    template_name='home/sal_amange.html'
+
+class BanhoView(TemplateView):
+    template_name='home/banho.html'
